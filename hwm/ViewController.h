bf7f60628a902f682c39ca09faed8c261c42d441
fuzixiang <fuzixiang@fuzixiangdeiMac.local>
2015-10-08 08:54:35 +0800

    Initial Commit

---
+++ hwm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hwm
+//
+//  Created by fuzixiang on 15/10/8.
+//  Copyright (c) 2015年 fuzixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hwm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hwm
+//
+//  Created by fuzixiang on 15/10/8.
+//  Copyright (c) 2015年 fuzixiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
